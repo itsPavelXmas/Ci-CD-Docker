1f3fa6928de6c2b868cbd3535eee1d15edd0237d
itsPavelXmas <pavelcristian2399@gmail.com>
2025-05-19 23:31:42 +0300

    added terraform

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "ec2-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name = "ec2-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "docker_ec2" {
+  ami                         = "ami-0c02fb55956c7d316" # Amazon Linux 2 in eu-central-1
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install docker -y
+              systemctl start docker
+              systemctl enable docker
+              EOF
+
+  tags = {
+    Name = "devops-lab-instance"
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.docker_ec2.public_ip
+}
